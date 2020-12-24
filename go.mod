28bb1f90fbc625ca353e4a632aecd667734db1af
eqinox76 <5861557+eqinox76@users.noreply.github.com>
2020-12-24 12:20:28 +0100

    feat: initial version

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/eqinox76/gotestor
+
+go 1.15
+
+require github.com/fsnotify/fsnotify v1.4.9
---
+++ main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"sync/atomic"
+	"time"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+var changedFlag uint32
+
+func main() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	go fileEventWatcher(watcher)
+	go testRunner()
+
+	err = discoverFiles(".", watcher)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	done := make(chan bool)
+	<-done
+}
+
+// searches the given path (not following symlinks) and watches all directories and go related files
+func discoverFiles(path string, watcher *fsnotify.Watcher) error {
+	validFilePattern := regexp.MustCompile(`(go\.mod)|(.*\.go)`)
+	return filepath.Walk(
+		path,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return nil
+			}
+
+			base := filepath.Base(path)
+			if info.IsDir() {
+				// ignore hidden directories like .git
+				if base != "." && strings.HasPrefix(base, ".") {
+					return filepath.SkipDir
+				}
+			} else if !validFilePattern.MatchString(filepath.Base(path)) {
+				return nil
+			}
+
+			err = watcher.Add(path)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			return nil
+		},
+	)
+}
+
+func fileEventWatcher(watcher *fsnotify.Watcher) {
+	for {
+		select {
+		case event, ok := <-watcher.Events:
+			if !ok {
+				return
+			}
+
+			info, err := os.Stat(event.Name)
+			if err == nil && info.IsDir() {
+				if event.Op&fsnotify.Create == fsnotify.Create ||
+					event.Op&fsnotify.Rename == fsnotify.Rename {
+					err := discoverFiles(event.Name, watcher)
+					if err != nil {
+						log.Fatal(err)
+					}
+				}
+				continue
+			}
+
+			atomic.StoreUint32(&changedFlag, 1)
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				return
+			}
+			log.Fatal("error:", err)
+		}
+	}
+}
+
+func runTest() {
+	args := append([]string{"test"}, os.Args[1:]...)
+	cmd := exec.Command("go", args...)
+
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	log.Println("running ", cmd.String())
+	err := cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func testRunner() {
+	buildTriggerd := false
+	for {
+		changed := atomic.LoadUint32(&changedFlag) == 1
+		atomic.StoreUint32(&changedFlag, 0)
+
+		if !buildTriggerd && !changed {
+			runTest()
+			buildTriggerd = true
+			continue
+		}
+		if buildTriggerd && changed {
+			buildTriggerd = false
+			time.Sleep(10 * time.Millisecond)
+			continue
+		}
+		time.Sleep(500 * time.Millisecond)
+	}
+}
